db4b72c3a8d8b0b603bdebcfec34b2039f915b60
Dimitri Hector <joshuahector86@gmail.com>
2022-11-15 23:30:44 -0800

    loading template for blog into project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import * as React from 'react'
 import ReactDOM from 'react-dom';
+
+import App from './Components/App'
+import Blog from './Blog/Blog'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Blog />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
